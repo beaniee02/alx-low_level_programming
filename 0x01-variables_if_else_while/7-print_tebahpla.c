8237af6da76130ab794ae043cbcdc031ce6031a2
busolami <busolamitemiloluwa02@gmail.com>
2022-09-16 03:10:39 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ * main - Prints alphabet in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char reverse;
+
+	for (reverse = 'z'; reverse <= 'a'; reverse--)
+		putchar(reverse);
+	putchar('\n');
+	return (0);
+}
