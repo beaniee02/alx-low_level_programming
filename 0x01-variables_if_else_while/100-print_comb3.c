98d54960eb9faba0bdfe6ecf237718afd2ad7df9
busolami <busolamitemiloluwa02@gmail.com>
2022-09-16 22:38:56 -0700

    comination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints numbers
+ *
+ * Description: Prints all possible differsnt combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, d;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (d = '0'; d <= '9'; d++)
+		{
+			if (i != d)
+			{
+				putchar(i);
+				putchar(d);
+				if (i == '8' && d == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
