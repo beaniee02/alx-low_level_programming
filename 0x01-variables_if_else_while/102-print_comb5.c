5b1e893585a373cb48cb8edbf6221cf68b296eda
busolami <busolamitemiloluwa02@gmail.com>
2022-09-17 04:40:19 -0700

    software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ * main - Prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, d;
+
+	for (i = '0'; i <= 98; i++)
+	{
+		for (d = i + 1; d <= 99; d++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((d / 10) + '0');
+			putchar((d % 10) + '0');
+
+			if (i == 98 && d == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
