61a6b86ee9cbb14499f32fd619f5602d4b9b4d3d
busolami <busolamitemiloluwa02@gmail.com>
2022-09-18 15:21:59 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * print_alphabet - prints alphabet and returns new line
+ * print_alphabet _X10 - print alphabet 10 times
+ * _islower - return 1 if c is lowercase, 0 otherwise
+ * _isalpha - return 1 ic c is upper or lowercase, 0 otherwise
+ */
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_X10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void jack_bauer(void);
+void times_table(int);
+void print_to_98(int);
+void print_times_table(void);
