99ffadb1242e2f88f460e11308645213069d0286
busolami <busolamitemiloluwa02@gmail.com>
2022-09-23 11:50:50 -0700

    comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2:second string to compare
+ * Return: The difference s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+	}
+	return (*s1 - *s2);
+}
